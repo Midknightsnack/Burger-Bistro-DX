3cf7777c812ed4422a8e47361e8ee09cb7ebd53e
Bradley Diep <bdippin12@csu.fullerton.edu>
2023-03-18 13:42:14 -0700

    review made but needs fixing

--- index.js
+++ index.js
@@ -1,58 +1,88 @@
-openModal();
-function openModal(){
-  var modal = document.getElementById("myModal");
-  var span = document.getElementsByClassName("close")[0];
-  document
-    .getElementById("calendar-days")
-    .addEventListener("click", function () {
-        modal.style.display = "block";
-    });
+const allStar = document.querySelectorAll('.rating .star')
+const ratingValue = document.querySelector('.rating input')
 
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
+allStar.forEach((item, idx) =>{
+  item.addEventListener('click', function (){
+    let click = 0
+    ratingValue.value = idx + 1
+    console.log(ratingValue.value)
 
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
+    allStar.forEach(i=>{
+      i.classList.replace('bxs-star', 'bx-star')
+      i.classList.remove('active')
+    })
+    for(let i=0; i < allStar.length; i++){
+      if(i <= idx){
+        allStar[i].classList.replace('bx-star', 'bxs-star')
+        allStar[i].classList.add('active')
+      } else{
+        allStar[i].style.setProperty('--i', click)
+        click++
       }
     }
-}
+  })
+})
 
-check();
-function check(){
-  document.getElementById('f1').innerHTML =  $(document).ready(function(){
-    $('#submit').click(function(){
-      var databack = $('#myModal #food').val();
-      $('#f1').html(databack);
-    }); 
-  });
+// myFunction();
+// function myFunction(){
+//   document.getElementById('thanks') = "Thank you for the rating"
+// }
 
-  document.getElementById('f2').innerHTML =  $(document).ready(function(){
-    $('#submit').click(function(){
-      var databack = $('#myModal #hours').val();
-      $('#f2').html(databack);
-    }); 
-  });
+// openModal();
+// function openModal(){
+//   var modal = document.getElementById("myModal");
+//   var span = document.getElementsByClassName("close")[0];
+//   document
+//     .getElementById("calendar-days")
+//     .addEventListener("click", function () {
+//         modal.style.display = "block";
+//     });
 
-  document.getElementById('f3').innerHTML =  $(document).ready(function(){
-    $('#submit').click(function(){
-      var databack = $('#myModal #minutes').val();
-      $('#f3').html(databack);
-    }); 
-  });
+//     span.onclick = function() {
+//       modal.style.display = "none";
+//     }
+
+//     window.onclick = function(event) {
+//       if (event.target == modal) {
+//         modal.style.display = "none";
+//       }
+//     }
+// }
+
+// check();
+// function check(){
+//   document.getElementById('f1').innerHTML =  $(document).ready(function(){
+//     $('#submit').click(function(){
+//       var databack = $('#myModal #food').val();
+//       $('#f1').html(databack);
+//     }); 
+//   });
+
+//   document.getElementById('f2').innerHTML =  $(document).ready(function(){
+//     $('#submit').click(function(){
+//       var databack = $('#myModal #hours').val();
+//       $('#f2').html(databack);
+//     }); 
+//   });
+
+//   document.getElementById('f3').innerHTML =  $(document).ready(function(){
+//     $('#submit').click(function(){
+//       var databack = $('#myModal #minutes').val();
+//       $('#f3').html(databack);
+//     }); 
+//   });
   
-  document.getElementById('f4').innerHTML =  $(document).ready(function(){
-    $('#submit').click(function(){
-      var databack = $('#myModal #tod').val();
-      $('#f4').html(databack);
-    }); 
-  });
+//   document.getElementById('f4').innerHTML =  $(document).ready(function(){
+//     $('#submit').click(function(){
+//       var databack = $('#myModal #tod').val();
+//       $('#f4').html(databack);
+//     }); 
+//   });
 
-  document.getElementById('f5').innerHTML =  $(document).ready(function(){
-    $('#submit').click(function(){
-      var databack = $('#myModal #notification_time').val();
-      $('#f5').html(databack);
-    }); 
-  });
-}
+//   document.getElementById('f5').innerHTML =  $(document).ready(function(){
+//     $('#submit').click(function(){
+//       var databack = $('#myModal #notification_time').val();
+//       $('#f5').html(databack);
+//     }); 
+//   });
+// }
